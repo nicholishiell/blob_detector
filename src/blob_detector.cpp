14c63df14d18bd2a303028c5405e237dd132a25f
Nicholi <nicholishiell@gmail.com>
2016-09-07 16:06:04 -0230

    Included second filter

--- src/blob_detector.cpp
+++ src/blob_detector.cpp
@@ -15,6 +15,13 @@ using namespace cv;
 
 float currentHeading = 0.;
 
+class BGR_Ranges{
+public:
+  float bMin, bMax;
+  float gMin, gMax;
+  float rMin, rMax;
+};
+
 void CurrentHeadingCallback(const std_msgs::Float64::ConstPtr& msg){
   currentHeading = msg->data;
 }
@@ -37,30 +44,43 @@ Point* FindCenteroid( Vector<Point> contour){
   return center;
 }
 
-int main(int argc, char **argv){
-  float centerX = 0.;
-  float centerY = 0.;
-
-  float bMin, bMax;
-  float gMin, gMax;
-  float rMin, rMax;
-  
+void ReadCalibData(BGR_Ranges& filter1, BGR_Ranges& filter2, float& roi_center_x, float& roi_center_y){
+ 
   fstream calibFile;
   
   calibFile.open("/home/pi/ns_catkin_ws/calibData/center_of_roi");
-  calibFile >> centerX;
-  calibFile >> centerY;
+  calibFile >> roi_center_x;
+  calibFile >> roi_center_y;
   calibFile.close();
   
   calibFile.open("/home/pi/ns_catkin_ws/calibData/bgr_ranges");
-  calibFile >> bMin;
-  calibFile >> bMax;
-  calibFile >> gMin;
-  calibFile >> gMax;
-  calibFile >> rMin;
-  calibFile >> rMax;
+  calibFile >> filter1.bMin;
+  calibFile >> filter1.bMax;
+  calibFile >> filter1.gMin;
+  calibFile >> filter1.gMax;
+  calibFile >> filter1.rMin;
+  calibFile >> filter1.rMax;
+
+  calibFile >> filter2.bMin;
+  calibFile >> filter2.bMax;
+  calibFile >> filter2.gMin;
+  calibFile >> filter2.gMax;
+  calibFile >> filter2.rMin;
+  calibFile >> filter2.rMax;
+  
   calibFile.close();
 
+}
+
+
+int main(int argc, char **argv){
+  float roi_center_x = 0.;
+  float roi_center_y = 0.;
+
+  BGR_Ranges filter1;
+  BGR_Ranges filter2;
+
+  ReadCalibData(filter1, filter2, roi_center_x, roi_center_y);
 
   ros::init(argc, argv, "blob_detector");
   ros::NodeHandle n;
@@ -72,91 +92,85 @@ int main(int argc, char **argv){
 
   raspicam::RaspiCam_Cv camera;
   cv::Mat rawImage;
-  cv::Mat blueImage;
+  cv::Mat blueImage, bin1,bin2;
   cv::Mat dilateImage;
   cv::Mat erodeImage;
-  
-  //camera.set( CV_CAP_PROP_EXPOSURE, 10); // Set shutter speed 100 for passive beacons)
+
+  cv::Mat erodeElement = getStructuringElement(MORPH_CROSS, Size( 5, 5 ), Point( 0, 0 ) );
+  cv::Mat dilateElement = getStructuringElement(MORPH_RECT, Size( 9, 9 ), Point( 0, 0 ) );
+
+  std_msgs::Float64MultiArray msgGlobal;     
+  std_msgs::Float64MultiArray msgLocal;     
+
+  //camera.set( CV_CAP_PROP_EXPOSURE, 10); // Set shutter speed 
  
   if ( !camera.open()) printf("Error opening camera\n");
 
-  bool keepGoing = true;
+ 
   int imageCounter = 0;
-  bool first = true;
-
-  while (ros::ok() and keepGoing){
+ 
+  while (ros::ok()){
     
     camera.grab();
     camera.retrieve(rawImage);
    
     // Create binary mask of blue regions and store it in blueImage  
-    inRange(rawImage, cv::Scalar(bMin,gMin,rMin), cv::Scalar(bMax,gMax,rMax), blueImage);
-    cv::imwrite("binaryMaskImage.jpg", blueImage);
-    cv::imwrite("rawImage.jpg", rawImage);
+    inRange(rawImage, cv::Scalar(filter1.bMin,filter1.gMin,filter1.rMin), cv::Scalar(filter1.bMax,filter1.gMax,filter1.rMax), bin1);
+    inRange(rawImage, cv::Scalar(filter2.bMin,filter2.gMin,filter2.rMin), cv::Scalar(filter2.bMax,filter2.gMax,filter2.rMax), bin2);
+    blueImage = bin1 + bin2;
 
-    Mat erodeElement = getStructuringElement(MORPH_CROSS, Size( 5, 5 ), Point( 0, 0 ) );
-    Mat dilateElement = getStructuringElement(MORPH_RECT, Size( 5, 5 ), Point( 0, 0 ) );
+    //cv::imwrite("binaryMaskImage.jpg", blueImage);
+    //cv::imwrite("rawImage.jpg", rawImage);
     
     erode(blueImage, erodeImage, erodeElement);
     dilate(erodeImage, dilateImage, dilateElement);
     
-    cv::imwrite("dilatedImage.jpg", dilateImage);
-    cv::imwrite("erodeImage.jpg", erodeImage);
+    //cv::imwrite("dilatedImage.jpg", dilateImage);
+    //cv::imwrite("erodeImage.jpg", erodeImage);
        
     /// Find contours   
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
-    RNG rng(12345);
+    
     findContours( dilateImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    /// Draw contours
-    Mat contourImage = Mat::zeros( blueImage.size(), CV_8UC3 );
-    for( int i = 0; i< contours.size(); i++ ){
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 
-	double a=contourArea( contours[i],false); 
+    /// Draw contours
+    //Mat contourImage = Mat::zeros( blueImage.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ ){
+      
+	double area =contourArea( contours[i],false); 
 	Point * center = FindCenteroid(contours[i]);
 	
-	if(a > 100.){
-	  drawContours( contourImage, contours, i, color, 2, 8, hierarchy, 0, Point() );
-	  float cx = center->x-centerX;
-	  float cy = center->y-centerY;
-	  printf("area = %f (x,y) = (%f, %f) dist = %f\n", a, cx, cy, sqrt(cx*cx+cy*cy));
+	if(area > 100.){
+
+	  float cx = center->x - roi_center_x;
+	  float cy = center->y - roi_center_y;
+	  
+	  // Use when forward is to the right in the image
+	  //float blobBearingLocal = atan2(blobY - centerY, blobX - centerX)*180./M_PI;
+	  float blobBearingLocal = atan2( -1.*cx, cy)*180./M_PI;
+
+	  float blobBearingGlobal = blobBearingLocal + currentHeading;
+	  if(blobBearingGlobal < -180.) blobBearingGlobal += 360.;
+	  if(blobBearingGlobal > 180.) blobBearingGlobal -= 360.;
+
+	  msgGlobal.data.push_back(blobBearingGlobal);
+	  msgLocal.data.push_back(blobBearingLocal); 
+
+	  //drawContours( contourImage, contours, i, Scalar(0,0,255), 2, 8, hierarchy, 0, Point() );
+	  //printf("area = %f (x,y) = (%f, %f) dist = %f\n", a, cx, cy, sqrt(cx*cx+cy*cy));
 	}
     }
-    cv::imwrite("contourTest.jpg", contourImage);
-    getchar();
     
-    /*std_msgs::Float64MultiArray msgGlobal;     
-    std_msgs::Float64MultiArray msgLocal;     
-    
-    msgGlobal.data.resize(keypoints.size()); 
-    msgLocal.data.resize(keypoints.size());  
- 
-    for(int i = 0; i < keypoints.size(); i++){
-      float blobX =  keypoints[i].pt.x;
-      float blobY = keypoints[i].pt.y; 
-      float blobSize = keypoints[i].size; 
-	
-      // Use when forward is to the right in the image
-      //float blobBearingLocal = atan2(blobY - centerY, blobX - centerX)*180./M_PI;
-
-      // Use when forward is down in the image
-      float blobBearingLocal = atan2( -1.*(blobX - centerX), blobY - centerY)*180./M_PI; 
-
-      float blobBearingGlobal = blobBearingLocal + currentHeading;
-      if(blobBearingGlobal < -180.) blobBearingGlobal += 360.;
-      if(blobBearingGlobal > 180.) blobBearingGlobal -= 360.;
-         
-      msgGlobal.data[i] = blobBearingGlobal;
-      msgLocal.data[i] = blobBearingLocal;
-      
-      //printf("%f\t%f\n", blobBearingGlobal, blobSize);
-      }
     global_pub.publish(msgGlobal);
     local_pub.publish(msgLocal);
-    //printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    */
-    
+
+    msgGlobal.data.clear();
+    msgLocal.data.clear();
+
+    //cv::imwrite("contourTest.jpg", contourImage);
+    //getchar();
+            
     ros::spinOnce();
     
     loop_rate.sleep();
